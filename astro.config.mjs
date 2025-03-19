c218d2c31f3e3c7ee838f74fdd3906f8130159a9
Marut <marutpun@gmail.com>
2025-03-19 12:16:33 +0700

    removed unused deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import yaml from '@rollup/plugin-yaml';
-
 import netlify from '@astrojs/netlify';
 
-import react from '@astrojs/react';
-
 export default defineConfig({
-  integrations: [mdx(), react()],
   vite: { plugins: [yaml(), tailwindcss()] },
   adapter: netlify(),
 });
