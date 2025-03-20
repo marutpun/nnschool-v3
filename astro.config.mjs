bb44780cff716275f3a33adca873430228975ba1
Marut <marutpun@gmail.com>
2025-03-20 10:25:16 +0700

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import yaml from '@rollup/plugin-yaml';
 import netlify from '@astrojs/netlify';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   vite: { plugins: [yaml(), tailwindcss()] },
   adapter: netlify(),
+  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })],
 });
