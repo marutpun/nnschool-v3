3e90114a751eeff30a440b5fcc67a160bbf599c0
Marut <marutpun@gmail.com>
2025-06-10 21:45:36 +0700

    remove partytown+analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import yaml from '@rollup/plugin-yaml';
 import netlify from '@astrojs/netlify';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   vite: { plugins: [yaml(), tailwindcss()] },
   adapter: netlify(),
-  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })],
 });
